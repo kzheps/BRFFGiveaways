4ae41e82ae72ad6a735dfd48d60a50ed4ab95774
Vsevolod Shoidlin <flovitskiy@gmail.com>
2025-05-31 17:33:55 +0800

    Add button OBS Overlay

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/overlay" element={<OBSOverlay />} />
